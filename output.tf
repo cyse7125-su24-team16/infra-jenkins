293b55c432959fea7350ed118fa9594a55cc15a8
VaishnaviNEU <mantri.v@northeastern.edu>
2024-05-28 11:51:04 -0400

    Pushing Code Changes.

--- output.tf
+++ output.tf
@@ -1,27 +1,34 @@
 output "region" {
   value = var.region
 }
-
+ 
 output "env" {
   value = var.env
 }
-
+ 
 output "Vpc_id" {
   value = aws_vpc.VPC.id
 }
-
+ 
 output "Public_Subnets" {
   value = aws_subnet.Public_Subnets[*].id
 }
-
+ 
 output "Private_subnets" {
   value = aws_subnet.Private_subnets[*].id
 }
-
+ 
 output "Igw_Id" {
   value = aws_internet_gateway.Igw.id
 }
-
+ 
 output "aws_security_group" {
   value = aws_security_group.Jenkins_Security_Group.id
+}
+ 
+output "aws_instance" {
+  value = aws_instance.Jenkins_Instance.id
+}
+output "ssh_key_name" {
+  value = aws_key_pair.Jenkins_Key.key_name
 }
